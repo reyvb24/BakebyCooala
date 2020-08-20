ae82839e7df24f68d280c310608e0488e4a28d6b
reyvb24 <61707645+reyvb24@users.noreply.github.com>
2020-08-20 15:27:20 +0700

    add navbar

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,28 @@
-var slideIndex = 0;
-showSlides();
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const header = document.querySelector(".header .container");
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+hamburger.addEventListener("click", ()=> {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
 }
+);
+
+document.addEventListener("touchmove", ScrollStart, false);
+document.addEventListener('gesturechange', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+// document.addEventListener('scroll', () => {
+//   var scrollposition = window.scrollY;
+//   if (scrollposition>300) {
+//     header.style.backgroundColor = '#ebcb9c';
+//   } else {
+//     header.style.backgroundColor = 'transparent';
+//   }
+// });
